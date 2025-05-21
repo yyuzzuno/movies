b9c8a821e6fd4bb599dcfa2e09174126f76a457b
yyuzzuno <i.yuzuha@outlook.jp>
2025-05-21 13:05:40 +0900

    型を定義 (#4)

---
+++ src/app/types.ts
@@ -0,0 +1,29 @@
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MoviesProps {
+  movies: Movie[];
+  isLoading: boolean;
+  isError: boolean;
+}
