5cee9071578dd9bc01111d4e14a98b998b55cd88
yyuzzuno <i.yuzuha@outlook.jp>
2025-05-22 22:55:21 +0900

    Feature/fix looks in pixel (#27)
    * マージンをbodyで設けるように変更 (#26)
    
    * ダークテーマの際への対応を外した
    
    * リセットCSSの導入 (#26)
    
    * 検索ボタンの改行を無効化 (#26)
    
    * テキスト表示のレイアウトを修正 (#26)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="mx-[32px]">
+      <main>
         <Searcher />
       </main>
     </QueryClientProvider>
